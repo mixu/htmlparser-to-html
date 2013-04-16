4440639b267d276fef6f0090b2758baa1ae5f351
Mikito Takada <mikito.takada@gmail.com>
2013-04-16 13:00:21 -0700

    Passthrough things that are undefined, or that do not have a type property

--- index.js
+++ index.js
@@ -42,37 +42,38 @@ function html(item) {
   if(Array.isArray(item)) {
     return item.map(html).join('');
   }
-  switch(item.type) {
-    case 'text':
-      return item.data;
-    case 'directive':
-      return '<'+item.data+'>';
-    case 'comment':
-      return '<!--'+item.data+'-->';
-    case 'style':
-    case 'script':
-    case 'tag':
-      var result = '<'+item.name;
-      if(item.attribs && Object.keys(item.attribs).length > 0) {
-        result += ' '+Object.keys(item.attribs).map(function(key){
-                return key + '="'+escapeAttrib(item.attribs[key])+'"';
-              }).join(' ');
-      }
-      if(item.children) {
-        result += '>'+html(item.children)+(emptyTags[item.name] ? '' : '</'+item.name+'>');
-      } else {
-        if(emptyTags[item.name]) {
-          result += '>';
-        } else {
-          result += '></'+item.name+'>';
+  if(typeof item != 'undefined' && typeof item.type != 'undefined') {
+    switch(item.type) {
+      case 'text':
+        return item.data;
+      case 'directive':
+        return '<'+item.data+'>';
+      case 'comment':
+        return '<!--'+item.data+'-->';
+      case 'style':
+      case 'script':
+      case 'tag':
+        var result = '<'+item.name;
+        if(item.attribs && Object.keys(item.attribs).length > 0) {
+          result += ' '+Object.keys(item.attribs).map(function(key){
+                  return key + '="'+escapeAttrib(item.attribs[key])+'"';
+                }).join(' ');
         }
-      }
-      return result;
-    case 'cdata':
-      return '<!CDATA['+item.data+']]>';
-    default:
-      return item;
+        if(item.children) {
+          result += '>'+html(item.children)+(emptyTags[item.name] ? '' : '</'+item.name+'>');
+        } else {
+          if(emptyTags[item.name]) {
+            result += '>';
+          } else {
+            result += '></'+item.name+'>';
+          }
+        }
+        return result;
+      case 'cdata':
+        return '<!CDATA['+item.data+']]>';
+    }
   }
+  return item;
 }
 
 module.exports = html;
